f16c724fd1f0ac38e0092271a43323bc73b1db58
Luis Y. <lyadiel882@gmail.com>
2024-10-30 08:47:29 -0400

    Homework_of_Lesson27.ino

--- Homework_of_Lesson27.ino
+++ Homework_of_Lesson27.ino
@@ -1,4 +1,4 @@
-// declaracion de variables --> LEDS!
+// Variable declaration --> LEDS!
 int redPinLED = 12;
 int greenPinLED = 11;
 int bluePinLED = 10;
@@ -9,30 +9,30 @@ int greenLEDStateNow = 0;
 int blueLEDStateNow = 0;
 int yellowLEDStateNow = 0;
 
-// declaracion de variables --> button pins!
+// Variable declaration --> button pins!
 int redButtonPin = 9;
 int greenButtonPin = 8;
 int blueButtonPin = 7;
 int yellowButtonPin = 6;
 
-int yellowButtonStateReadNow = 1; // valor que se leera digitalmente del buttonPin
+int yellowButtonStateReadNow = 1; // value to be read digitally from the buttonPin
 int yellowButtonStateOld = 1;
 
-int redButtonStateReadNow = 1; // valor que se leera digitalmente del buttonPin
+int redButtonStateReadNow = 1; // value to be read digitally from the buttonPin
 int redButtonStateOld = 1;
 
-int greenButtonStateReadNow = 1; // valor que se leera digitalmente del buttonPin
+int greenButtonStateReadNow = 1; // value to be read digitally from the buttonPin
 int greenButtonStateOld = 1;
 
-int blueButtonStateReadNow = 1; // valor que se leera digitalmente del buttonPin
+int blueButtonStateReadNow = 1; // value to be read digitally from the buttonPin
 int blueButtonStateOld = 1;
 
-int br = 115200; // para el Serial monitor
+int br = 115200; // for the Serial monitor
 
 int endingLoop = 50; // delay
 
 void setup() {
-  // Seteo los LEDs pins, button pins, y el serial monitor
+  // Set the LEDs pins, button pins, and the serial monitor
   pinMode (redButtonPin, INPUT_PULLUP);
   pinMode (greenButtonPin, INPUT_PULLUP);
   pinMode (blueButtonPin, INPUT_PULLUP);
@@ -52,20 +52,21 @@ void setup() {
 }
 
 void loop() {
-  // hago el procedimiento
-  // primero se lee el valor de los button switchs
+  // Perform the procedure
+  // First, read the value from the button switches
   redButtonStateReadNow = digitalRead(redButtonPin);
   greenButtonStateReadNow = digitalRead(greenButtonPin);
   blueButtonStateReadNow = digitalRead(blueButtonPin);
   yellowButtonStateReadNow = digitalRead(yellowButtonPin);
-  // if statements para encender el RGB LED dependiendo si presiono el button switch o no. Los colores son: RED, GREEN AND BLUE
-  // Se empieza con el ROJO
-  if (redButtonStateOld == 1 && redButtonStateReadNow == 0) { // cuando SI presiono el button switch
+  
+  // if statements to turn on the RGB LED depending on whether the button switch is pressed or not. The colors are: RED, GREEN AND BLUE
+  // Start with RED
+  if (redButtonStateOld == 1 && redButtonStateReadNow == 0) { // when the button switch IS pressed
 
   if (redLEDStateNow == 0) {
     Serial.println("The red LED is ON.");
     Serial.println("");
-    digitalWrite (redPinLED, HIGH); // el pin 9 del button switch va a reconocer el voltage drop del 10k resistor, por lo cual va a haber voltaje (el LED SI va a prender)
+    digitalWrite (redPinLED, HIGH); // pin 9 of the button switch will recognize the voltage drop from the 10k resistor, so there will be voltage (the LED WILL turn on)
   }
 
   if (redLEDStateNow == 1) {
@@ -74,20 +75,20 @@ void loop() {
     digitalWrite (redPinLED, LOW);
   }
 
-  redLEDStateNow = !redLEDStateNow; // --> se hace la inversion de 0 a 1 y de 1 a 0. Siempre se tiene que trabajar con la logica de los button switches
-  // y en el final se cambia la logica de los LEDs para que se pueda implementar correctamente
+  redLEDStateNow = !redLEDStateNow; // --> invert from 0 to 1 and from 1 to 0. Always work with the logic of the button switches
+  // and at the end change the logic of the LEDs so they can be implemented correctly
   }
 
-  redButtonStateOld = redButtonStateReadNow; // la accion pasada del red button switch se cambia con la que se hizo reciente para que
-  // los cambios se actualizen en cada loop
+  redButtonStateOld = redButtonStateReadNow; // the previous action of the red button switch is changed with the recent one so
+  // the changes are updated in each loop
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-  // VERDE
-  if (greenButtonStateOld == 1 && greenButtonStateReadNow == 0) { // cuando SI presiono el button switch
+  // GREEN
+  if (greenButtonStateOld == 1 && greenButtonStateReadNow == 0) { // when the button switch IS pressed
 
   if (greenLEDStateNow == 0) {
     Serial.println("The green LED is ON.");
     Serial.println("");
-    digitalWrite (greenPinLED, HIGH); // el pin 9 del button switch va a reconocer el voltage drop del 10k resistor, por lo cual va a haber voltaje (el LED SI va a prender)
+    digitalWrite (greenPinLED, HIGH); // pin 9 of the button switch will recognize the voltage drop from the 10k resistor, so there will be voltage (the LED WILL turn on)
   }
 
   if (greenLEDStateNow == 1) {
@@ -96,20 +97,20 @@ void loop() {
     digitalWrite (greenPinLED, LOW);
   }
 
-  greenLEDStateNow = !greenLEDStateNow; // --> se hace la inversion de 0 a 1 y de 1 a 0. Siempre se tiene que trabajar con la logica de los button switches
-  // y en el final se cambia la logica de los LEDs para que se pueda implementar correctamente
+  greenLEDStateNow = !greenLEDStateNow; // --> invert from 0 to 1 and from 1 to 0. Always work with the logic of the button switches
+  // and at the end change the logic of the LEDs so they can be implemented correctly
   }
 
-  greenButtonStateOld = greenButtonStateReadNow; // la accion pasada del red button switch se cambia con la que se hizo reciente para que
-  // los cambios se actualizen en cada loop
+  greenButtonStateOld = greenButtonStateReadNow; // the previous action of the red button switch is changed with the recent one so
+  // the changes are updated in each loop
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------
-  // AZUL
-  if (blueButtonStateOld == 1 && blueButtonStateReadNow == 0) { // cuando SI presiono el button switch
+  // BLUE
+  if (blueButtonStateOld == 1 && blueButtonStateReadNow == 0) { // when the button switch IS pressed
 
   if (blueLEDStateNow == 0) {
     Serial.println("The blue LED is ON.");
     Serial.println("");
-    digitalWrite (bluePinLED, HIGH); // el pin 9 del button switch va a reconocer el voltage drop del 10k resistor, por lo cual va a haber voltaje (el LED SI va a prender)
+    digitalWrite (bluePinLED, HIGH); // pin 9 of the button switch will recognize the voltage drop from the 10k resistor, so there will be voltage (the LED WILL turn on)
   }
 
   if (blueLEDStateNow == 1) {
@@ -118,20 +119,20 @@ void loop() {
     digitalWrite (bluePinLED, LOW);
   }
 
-  blueLEDStateNow = !blueLEDStateNow; // --> se hace la inversion de 0 a 1 y de 1 a 0. Siempre se tiene que trabajar con la logica de los button switches
-  // y en el final se cambia la logica de los LEDs para que se pueda implementar correctamente
+  blueLEDStateNow = !blueLEDStateNow; // --> invert from 0 to 1 and from 1 to 0. Always work with the logic of the button switches
+  // and at the end change the logic of the LEDs so they can be implemented correctly
   }
 
-  blueButtonStateOld = blueButtonStateReadNow; // la accion pasada del red button switch se cambia con la que se hizo reciente para que
-  // los cambios se actualizen en cada loop
+  blueButtonStateOld = blueButtonStateReadNow; // the previous action of the red button switch is changed with the recent one so
+  // the changes are updated in each loop
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   // YELLOW
-  if (yellowButtonStateOld == 1 && yellowButtonStateReadNow == 0) { // cuando SI presiono el button switch
+  if (yellowButtonStateOld == 1 && yellowButtonStateReadNow == 0) { // when the button switch IS pressed
 
   if (yellowLEDStateNow == 0) {
     Serial.println("The yellow LED is ON.");
     Serial.println("");
-    digitalWrite (yellowPinLED, HIGH); // el pin 9 del button switch va a reconocer el voltage drop del 10k resistor, por lo cual va a haber voltaje (el LED SI va a prender)
+    digitalWrite (yellowPinLED, HIGH); // pin 9 of the button switch will recognize the voltage drop from the 10k resistor, so there will be voltage (the LED WILL turn on)
   }
 
   if (yellowLEDStateNow == 1) {
@@ -140,12 +141,12 @@ void loop() {
     digitalWrite (yellowPinLED, LOW);
   }
 
-  yellowLEDStateNow = !yellowLEDStateNow; // --> se hace la inversion de 0 a 1 y de 1 a 0. Siempre se tiene que trabajar con la logica de los button switches
-  // y en el final se cambia la logica de los LEDs para que se pueda implementar correctamente
+  yellowLEDStateNow = !yellowLEDStateNow; // --> invert from 0 to 1 and from 1 to 0. Always work with the logic of the button switches
+  // and at the end change the logic of the LEDs so they can be implemented correctly
   }
 
-  yellowButtonStateOld = yellowButtonStateReadNow; // la accion pasada del red button switch se cambia con la que se hizo reciente para que
-  // los cambios se actualizen en cada loop
+  yellowButtonStateOld = yellowButtonStateReadNow; // the previous action of the red button switch is changed with the recent one so
+  // the changes are updated in each loop
 
-  delay (endingLoop); // se aplica delay para que no hayan errores pequeños en la leida de los button switches y en la transmiscion de señal del RGB LED
+  delay (endingLoop); // apply delay to avoid small errors in reading the button switches and in the signal transmission of the RGB LED
 }
